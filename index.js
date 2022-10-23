1904fb6f72883fec406d90e428cfb60ad2fca22c
Jafethpc <hjpachecocampos@gmail.com>
2022-10-23 14:32:30 -0400

    Application is now working and tested

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   const data = fs.readFileSync(__dirname + "/data.json");
 
   // PARSED JSON
-  const parsedData = JSON.parse(data);
+  const parsedData = JSON.parse(data).activeTasks;
 
   res.render("index", { tasks: parsedData });
 });
@@ -36,7 +36,7 @@ app.post("/add", (req, res) => {
   const parsedData = JSON.parse(data);
 
   // PUSHING DATA TO JSON FILE
-  parsedData.push({
+  parsedData.activeTasks.push({
     task: `${req.body.task}`,
     day: `${req.body.day}`,
     month: `${req.body.month}`,
@@ -61,9 +61,12 @@ app.post("/save/:id", (req, res) => {
   const data = fs.readFileSync(__dirname + "/data.json");
 
   const parsedData = JSON.parse(data);
-  parsedData.find((e) => {
+  parsedData.activeTasks.find((e) => {
     if (e.id == req.params.id) {
-      e.task = req.body.editedTask;
+      e.task = req.body.task;
+      e.day = req.body.day;
+      e.month = req.body.month;
+      console.log(e);
       const stringifiedData = JSON.stringify(parsedData);
       fs.writeFileSync("data.json", stringifiedData, (err) => {
         if (err) throw err;
@@ -79,11 +82,29 @@ app.post("/delete/:id", (req, res) => {
   const data = fs.readFileSync(__dirname + "/data.json");
 
   const parsedData = JSON.parse(data);
-  console.log(parsedData);
-  parsedData.find((e, i) => {
+  parsedData.activeTasks.find((e, i) => {
     console.log(e.id, req.params.id);
     if (e.id == req.params.id) {
-      parsedData.splice(i, 1);
+      parsedData.activeTasks.splice(i, 1);
+      const stringifiedData = JSON.stringify(parsedData);
+      fs.writeFileSync("data.json", stringifiedData, (err) => {
+        if (err) throw err;
+        console.log("new data added");
+      });
+      res.redirect("/");
+    }
+  });
+});
+
+// COMPLETE ROUTE
+app.post("/complete/:id", (req, res) => {
+  const data = fs.readFileSync(__dirname + "/data.json");
+
+  const parsedData = JSON.parse(data);
+  parsedData.activeTasks.find((e, i) => {
+    if (e.id == req.params.id) {
+      parsedData.completedTasks.push(e);
+      parsedData.activeTasks.splice(i, 1);
       const stringifiedData = JSON.stringify(parsedData);
       fs.writeFileSync("data.json", stringifiedData, (err) => {
         if (err) throw err;
--- public/tt.js
+++ public/tt.js
@@ -1,17 +1,41 @@
 const editBtn = document.querySelectorAll(".editBtn");
-const saveInput = document.querySelectorAll(".saveinput");
-const saveBtn = document.querySelectorAll(".saveBtn");
-const task = document.querySelectorAll(".taskTitle");
+const editContainer = document.querySelectorAll(".editContainer");
 
 for (let i = 0; i < editBtn.length; i++) {
   editBtn[i].addEventListener("click", () => {
-    saveInput[i].style.opacity = 100;
-    saveInput[i].style.display = "block";
-    saveBtn[i].style.opacity = 100;
-    saveBtn[i].style.display = "block";
-    editBtn[i].style.opacity = 0;
-    editBtn[i].style.display = "none";
-    task[i].style.opacity = 0;
-    task[i].style.display = "none";
+    const heightProp = window
+      .getComputedStyle(editContainer[i])
+      .getPropertyValue("height");
+    if (heightProp === "0px") {
+      editContainer[i].style.height = "170px";
+    } else if (heightProp === "170px") {
+      editContainer[i].style.height = "0px";
+    }
   });
 }
+
+const addTaskBtn = document.querySelector(".addTaskBtn");
+
+const addContainer = document.querySelector(".addContainer");
+
+addTaskBtn.addEventListener("click", () => {
+  const heightProp = window
+    .getComputedStyle(addContainer)
+    .getPropertyValue("height");
+
+  if (heightProp === "0px") {
+    addContainer.style.height = "170px";
+    addTaskBtn.innerHTML = "-";
+    addTaskBtn.style.padding = "0 0 5px 0";
+  } else if (heightProp === "170px") {
+    addContainer.style.height = "0px";
+    addTaskBtn.innerHTML = "+";
+    addTaskBtn.style.padding = "0";
+  }
+});
+
+const addBtn = document
+  .querySelector(".addBtn")
+  .addEventListener("click", () => {
+    addContainer.style.height = "0px";
+  });
